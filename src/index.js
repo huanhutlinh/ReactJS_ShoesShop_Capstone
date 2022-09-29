297de6f22f1507cfddded80968ddff1b3b4cfbc9
huanhutlinh <huanhutlinh128@gmail.com>
2022-09-29 19:45:19 +0700

    Revert "Setup environment to develop web"
    This reverts commit 5e0a0158ff0456d1edc9392532c9c826960b1cb2.

--- src/index.js
+++ src/index.js
@@ -1,34 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './assets/scss/style.scss';
-import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
-import HomeTemplate from './templates/HomeTemplate';
-import Carts from './pages/Carts/Carts';
-import Detail from './pages/Detail/Detail';
-import Home from  './pages/Home/Home';
-import Login from './pages/Login/Login';
-import Profile from './pages/Profile/Profile';
-import Register from './pages/Register/Register';
-import Search from './pages/Search/Search';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <BrowserRouter>
-      <Routes>
-        <Route path='' element={<HomeTemplate/>}>
-            <Route index element={<Home/>}></Route>
-            <Route path='register' element={<Register/>}></Route>
-            <Route path='login' element={<Login/>}></Route>
-            <Route path='search' element={<Search/>}></Route>
-            <Route path='detail'>
-              <Route path=':id' element={<Detail/>}></Route>
-            </Route>
-            <Route path='profile' element={<Profile/>}></Route>
-            <Route path='cart' element={<Carts/>}></Route>
-            <Route path='*' element={<Navigate to=""/>}></Route>
-        </Route>
-      </Routes>
-    </BrowserRouter>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
-
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
