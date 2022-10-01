1e101ad58ff00f4659d56c9abede4503e86d3e2d
Linhdong <kn.durian.sweet@gmail.com>
2022-10-01 15:57:26 +0800

    add data product for carousel
    Signed-off-by: Linhdong <kn.durian.sweet@gmail.com>

--- src/redux/Reducers/productReducer.jsx
+++ src/redux/Reducers/productReducer.jsx
@@ -4,7 +4,27 @@ import {http} from '../../util/config'
 
 
 const initialState = {
-    dataProduct: []
+    dataProduct: [],
+    dataCarousel: [
+      {
+        "id": 1,
+        "name": "Adidas Prophere",
+        "shortDescription": "The midsole contains 20% more Boost for an amplified Boost feeling.\r\n\r\n",
+        "image": "https://shop.cyberlearn.vn/images/adidas-prophere.png"
+      },
+      {
+        "id": 2,
+        "name": "Adidas Swift Run",
+        "shortDescription": "The midsole contains 20% more Boost for an amplified Boost feeling.\r\n\r\n",
+        "image": "https://shop.cyberlearn.vn/images/adidas-swift-run.png"
+      },
+      {
+        "id": 3,
+        "name": "Adidas Ultraboost 4",
+        "shortDescription": "The adidas Primeknit upper wraps the foot with a supportive fit that enhances movement.\r\n\r\n",
+        "image": "https://shop.cyberlearn.vn/images/adidas-ultraboost-4.png"
+      },
+    ]
 }
 
 const productReducer = createSlice({
