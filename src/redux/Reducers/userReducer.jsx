2b09969b19b40e78e26535f404c2e4f140cd69b6
huanhutlinh <huanhutlinh128@gmail.com>
2022-10-03 13:01:25 +0700

    Handle Register

---
+++ src/redux/Reducers/userReducer.jsx
@@ -0,0 +1,65 @@
+import { createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import {
+  ACCESS_TOKEN,
+  setStore,
+  setStoreJSON,
+  USER_LOGIN,
+} from "../../util/config";
+
+const initialState = {
+  userLogin: localStorage.getItem(USER_LOGIN)
+    ? JSON.parse(localStorage.getItem(USER_LOGIN))
+    : null,
+};
+
+const userReducer = createSlice({
+  name: "userReducer",
+  initialState,
+  reducers: {
+    setUserLoginAction: (state, action) => {
+      state.userLogin = action.payload;
+    },
+  },
+});
+
+export const { setUserLoginAction } = userReducer.actions;
+
+export default userReducer.reducer;
+
+// -----------Action Api--------------
+
+export const signInAPI = (userLogin) => {
+  return async (dispatch) => {
+    try {
+      let result = await axios({
+        url: "https://shop.cyberlearn.vn/api/Users/signin",
+        method: "POST",
+        data: userLogin,
+      });
+
+      setStore(ACCESS_TOKEN, result.data.content.accessToken);
+      setStoreJSON(USER_LOGIN, result.data.content);
+
+      const action = setUserLoginAction(result.data.content);
+      dispatch(action);
+    } catch (err) {
+      alert(err.response.data.message);
+    }
+  };
+};
+
+export const postOderAPI = async (order) => {
+  try {
+    const result = await axios({
+      url: "https://shop.cyberlearn.vn/api/Users/order",
+      method: "POST",
+      data: order,
+    });
+
+    console.log(result);
+  } catch (err) {
+    console.log(err);
+  }
+};
